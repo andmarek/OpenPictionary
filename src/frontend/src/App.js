82a48ce5795958f891f897d3df46543df266b915
andmarek <andmarek@outlook.com>
2019-11-11 00:29:32 -0600

    navbar partially working (needs to be styled)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,18 +5,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './scenes/Home.js';
 
 /* Components */
-import { Navbar } from './components/navbar.js';
+import { NavBar } from './components/navbar.js';
 
 /* Misc */
 import './App.css';
 
 /* Assets */
-import logo from './logo.svg';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
+      <NavBar />
       <Router>
         <Switch>
          <Route exact path = "/" component={Home}/>
--- src/frontend/src/components/navbar.js
+++ src/frontend/src/components/navbar.js
@@ -3,21 +3,28 @@ import styled from 'styled-components';
 
 //remember to add styled components somewhere
 const Styles = styled.div`
-    .nav {
-        background-color: #222;
+    .navbar {
+        display: flex;
+        padding: 1px;
+        float: left;
+        width: 99%;
+        justify-content: space-between;
+        background-color: #D68266;
+        margin: 0;
+        padding: .5em;
     }
+
+    p {
+        #FFFFFF;
+    }
+
 `;
 
-export const NavBar = React.createClass({
-    render() {
-        return (
+export const NavBar = () => (
             <Styles>
-                <div class="nav">
+                <div class="navbar">
                   <ul>
                      <p> test </p>
                   </ul> </div>
             </Styles>
-        )
-    }
-});
-
+)
