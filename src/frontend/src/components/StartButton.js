c06a0f16b5837f77a7bd3d3bf136d1703c9e0cef
andmarek <andmarek@outlook.com>
2019-11-18 14:51:54 -0600

    fixed some things but still need the SVG to be white

---
+++ src/frontend/src/components/StartButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Styles = styled.div`
+
+`;
+class StartButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    };
+    render () {
+        render (
+           <div id="button">
+              <p>Start Drawing</p> 
+           </div> 
+        );
+    }
+}
+
+export default StartButton;
+
