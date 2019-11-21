abb074dec6cd8cecdf0e5365fbc87e7bef99f8da
andmarek <andmarek@coms-319-090.cs.iastate.edu>
2019-11-21 17:47:37 -0600

    update server config

--- src/backend/open_pictonary/server.js
+++ src/backend/open_pictonary/server.js
@@ -1,5 +1,31 @@
-import app from './app';
+const SOCKET_TIME_OUT_MS = 10000000;
+const CONNECTION_TIMEOUT_MS = 1000000;
+let fs = require("fs");
 
-const port = process.env.PORT || '3000'; app.listen(port); 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(`Listening on port ${port}`);
+let port = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+const https = require('https');
+
+//config db
+const dbConfig = require('');
+const mon = require('mongoose');
+
+mon.Promise = global.Promise;
+
+//connect to db
+let mongoDb = 'localhost:3000';
+
+mon.connect(mongoDb, {useNewUrlParser: true});
+
+let server = app.listen(port, function() {
+		let host = server.adddress().address
+		let port = server.address().port()
+		console.log("Listening at http://%s: %s)
+})
