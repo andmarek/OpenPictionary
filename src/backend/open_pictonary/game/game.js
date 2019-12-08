a6db7e1b748a4c4ae0c6725cb8a34c5599c2c48f
CMaster20 <colbym@iastate.edu>
2019-12-08 12:52:32 -0600

    adding logic to guess and message

--- src/backend/open_pictonary/game/game.js
+++ src/backend/open_pictonary/game/game.js
@@ -14,6 +14,22 @@ exports.onConnect = (socket) => {
         console.info(`Socket ${socket.id} has disconnected.`);
     });
 
+    // Guess
+    socket.on("guess", (guess) => {
+        console.info(`Socket ${socket.id} has guessed ${guess}.`);
+        var msg = "incorrect";
+        if (guess === 'TRUE') {
+            msg = "correct"
+            socket.boardcast.emit(`${socket.id} has correctly guess this round`)
+        } 
+        socket.emit(msg);
+    });
+
+    // Message
+    socket.on("message", (message) => {
+        socket.boardcast.emit(message);
+    });
+
     // echoes on the terminal every "hello" message this socket sends
     socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: "${helloMsg}"`));
 
