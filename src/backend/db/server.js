61a1cd524df982dee16aac0bbe46445eacd17894
andmarek <andmarek@outlook.com>
2019-12-10 15:41:29 -0600

    finished websockets... the formatting is wack af tho

--- src/backend/db/server.js
+++ src/backend/db/server.js
@@ -4,13 +4,28 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const cors = require('cors'); // Please don't remove this. It's a headache.
+const WebSocket = require('ws');
 
 const routes = require('./routes/user-router');
 
 const apiPort = 8080;
 
+const wss = new WebSocket.Server({ port: 3030 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
 app.use(bodyParser.urlencoded({extended: true}))
+
 app.use(cors());
+
 app.use(express.json());
 
 let mongo_uri = 'mongodb://andrew:password@coms-319-090.cs.iastate.edu:27017/open_pictionary'
