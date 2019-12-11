b2db96d6ba813d3f333694273597d7c0fccb45be
andmarek <andmarek@outlook.com>
2019-12-11 16:06:55 -0600

    updated server i think

--- src/backend/db/server.js
+++ src/backend/db/server.js
@@ -19,15 +19,21 @@ const apiPort = 8080;
 const wss = new WebSocket.Server({ port: 3030 });
 
 let mongo_uri = 'mongodb://andrew:password@coms-319-090.cs.iastate.edu:27017/open_pictionary'
+let prev_data = [];
+let current_word = "";
 
 wss.on('connection', function connection(ws) {
-    /* Handles messages from the server
-    These will generally be chat or otherwise */
+    /* Handling previous messages */
+    for(let i = 0; i < prev_data.length; i++) {
+        ws.send(prev_data[i]);
+    }
+    /* Handle any messages */
     ws.on('message', function incoming(data) {
         console.log('message');
+        prev_data.push(data);
         wss.clients.forEach(function each(client) {
+            console.log(data);
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                console.log(data);
                 client.send(data);
             }
         });
@@ -46,6 +52,37 @@ app.use(session({
     saveUninitialized: true,
     store: new MongoStore({ mongooseConnection: db})
 }));
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !res.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+})
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/home');
+    } else {
+        next();
+    }
+}
+
+app.route('/register')
+    .post((req, res) => {
+        routes.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(user => {
+                req.session.user = user.dataValues;
+                res.redirect('/profile');
+            })
+            .catch(error => {
+                res.redirect('/register');
+            });
+    });
 
 app.use(express.json());
 
