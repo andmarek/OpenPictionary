e3311be8515c4088bbb662fbd1aa391a1f072528
andmarek <andmarek@outlook.com>
2019-12-10 22:43:55 -0600

    added store

--- src/backend/db/server.js
+++ src/backend/db/server.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-const cors = require('cors'); // Please don't remove this. It's a headache.
-const WebSocket = require('ws');
+const cors = require('cors'); // Please don't remove this. It's a headache.  const WebSocket = require('ws');
+
+/* Sessions allow the user to login correctly */
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+const MongoStore = require('connect-mongo')(session);
 
 const routes = require('./routes/user-router');
 
 const apiPort = 8080;
 
 const wss = new WebSocket.Server({ port: 3030 });
+
+let mongo_uri = 'mongodb://andrew:password@coms-319-090.cs.iastate.edu:27017/open_pictionary'
 
 wss.on('connection', function connection(ws) {
     /* Handles messages from the server
@@ -26,18 +33,26 @@ wss.on('connection', function connection(ws) {
     });
 });
 
-app.use(bodyParser.urlencoded({extended: true}))
+var db = mongoose.connection;
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
+app.use(cookieParser())
+
+app.use(session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: db})
+    }));
 
 app.use(express.json());
 
-let mongo_uri = 'mongodb://andrew:password@coms-319-090.cs.iastate.edu:27017/open_pictionary'
+//mongoose.Promise = global.Promise;
 
 mongoose.connect(mongo_uri,
-    {useNewUrlParser: true, useUnifiedTopology: true}).catch( e => { console.error('Connection error', e.message)});
+    {useNewUrlParser: true, useUnifiedTopology: true}).catch( e => { console.error('Connection error', e.message)}).then(console.log("wtf"));
 
-var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "MongoDB connection error:" )); // investigate
 
